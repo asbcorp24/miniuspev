b478f83d4026b5a4c3df847a8d9981100ee10911
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-01 20:47:35 +0300

    Add Subject model

---
+++ app/Models/Subject.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Subject extends Model
+{
+    protected $fillable = ['name', 'code'];
+    public function lessons(): HasMany { return $this->hasMany(Lesson::class); }
+}
