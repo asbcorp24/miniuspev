87697069556f3ae9b9f72c4438fac0f3ad3946bc
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-01 20:46:54 +0300

    Add SQLite database configuration

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Str;
+
+return [
+    'default' => env('DB_CONNECTION', 'sqlite'),
+    'connections' => [
+        'sqlite' => [
+            'driver' => 'sqlite',
+            'url' => env('DATABASE_URL'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+    ],
+    'migrations' => 'migrations',
+    'redis' => [
+        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'options' => [
+            'cluster' => env('REDIS_CLUSTER', 'redis'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'miniuspev'), '_').'_database_'),
+        ],
+    ],
+];
