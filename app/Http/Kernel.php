c11c6853c97e8294c1be97cdcbfad49f80256f79
asbcorp24 <69872347+asbcorp24@users.noreply.github.com>
2026-09-06 21:17:46 +0300

    Register auth middleware alias

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends HttpKernel
     ];
 
     protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
     ];
